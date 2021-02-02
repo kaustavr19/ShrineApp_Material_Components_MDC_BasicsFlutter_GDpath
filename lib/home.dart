dc03fea1265d088345c893fbf511633f4dc94075
kaustavr19 <kaustavr19@users.noreply.github.com>
2021-02-02 23:15:00 +0530

    MDC-102 multiplied card into collection

--- lib/home.dart
+++ lib/home.dart
@@ -2,6 +2,37 @@ import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
   @override
+  List<Card> _buildGridCards(int count) {
+    List<Card> cards = List.generate(
+        count,
+        (int index) => Card(
+              clipBehavior: Clip.antiAlias,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  AspectRatio(
+                    aspectRatio: 18.0 / 11.0,
+                    child: Image.asset('assets/diamond.png'),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.fromLTRB(16.0, 12.0, 16.0, 8.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        Text('Title'),
+                        SizedBox(
+                          height: 8.0,
+                        ),
+                        Text('Secondary Text'),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ));
+    return cards;
+  }
+
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
@@ -37,37 +68,11 @@ class HomePage extends StatelessWidget {
         ),
       ),
       body: GridView.count(
-        crossAxisCount: 2,
-        padding: EdgeInsets.all(16.0),
-        childAspectRatio: 8.0 / 9.0,
-        children: <Widget>[
-          Card(
-            clipBehavior: Clip.antiAlias,
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: <Widget>[
-                AspectRatio(
-                  aspectRatio: 18.0 / 11.0,
-                  child: Image.asset('assets/diamond.png'),
-                ),
-                Padding(
-                  padding: EdgeInsets.fromLTRB(16.0, 12.0, 16.0, 8.0),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: <Widget>[
-                      Text('Title'),
-                      SizedBox(
-                        height: 8.0,
-                      ),
-                      Text('Secondary Text'),
-                    ],
-                  ),
-                ),
-              ],
-            ),
+          crossAxisCount: 2,
+          padding: EdgeInsets.all(16.0),
+          childAspectRatio: 8.0 / 9.0,
+          children: _buildGridCards(10) // Replace
           ),
-        ],
-      ),
       resizeToAvoidBottomInset: false,
     );
   }
