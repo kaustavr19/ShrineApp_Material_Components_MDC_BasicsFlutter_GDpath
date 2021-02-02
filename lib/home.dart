df904b0e5d97d6a56f639fe9f8e7926afbd54ce8
kaustavr19 <kaustavr19@users.noreply.github.com>
2021-02-02 23:29:02 +0530

    MDC-102 product data added

--- lib/home.dart
+++ lib/home.dart
@@ -5,35 +5,60 @@ import 'model/product.dart';
 
 class HomePage extends StatelessWidget {
   @override
-  List<Card> _buildGridCards(int count) {
-    List<Card> cards = List.generate(
-        count,
-        (int index) => Card(
-              clipBehavior: Clip.antiAlias,
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                  AspectRatio(
-                    aspectRatio: 18.0 / 11.0,
-                    child: Image.asset('assets/diamond.png'),
-                  ),
-                  Padding(
-                    padding: EdgeInsets.fromLTRB(16.0, 12.0, 16.0, 8.0),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: <Widget>[
-                        Text('Title'),
-                        SizedBox(
-                          height: 8.0,
-                        ),
-                        Text('Secondary Text'),
-                      ],
-                    ),
-                  ),
-                ],
+  List<Card> _buildGridCards(BuildContext context) {
+    List<Product> products = ProductsRepository.loadProducts(Category.all);
+
+    if (products == null || products.isEmpty) {
+      return const <Card>[];
+    }
+
+    final ThemeData theme = Theme.of(context);
+    final NumberFormat formatter = NumberFormat.simpleCurrency(
+        locale: Localizations.localeOf(context).toString());
+
+    return products.map((product) {
+      return Card(
+        clipBehavior: Clip.antiAlias,
+        // TODO: Adjust card heights (103)
+        child: Column(
+          // TODO: Center items on the card (103)
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            AspectRatio(
+              aspectRatio: 18 / 11,
+              child: Image.asset(
+                product.assetName,
+                package: product.assetPackage,
+                // TODO: Adjust the box size (102)
               ),
-            ));
-    return cards;
+            ),
+            Expanded(
+              child: Padding(
+                padding: EdgeInsets.fromLTRB(16.0, 12.0, 16.0, 8.0),
+                child: Column(
+                  // TODO: Align labels to the bottom and center (103)
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  // TODO: Change innermost Column (103)
+                  children: <Widget>[
+                    // TODO: Handle overflowing labels (103)
+                    Text(
+                      product.name,
+                      style: theme.textTheme.title,
+                      maxLines: 1,
+                    ),
+                    SizedBox(height: 8.0),
+                    Text(
+                      formatter.format(product.price),
+                      style: theme.textTheme.body2,
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      );
+    }).toList();
   }
 
   Widget build(BuildContext context) {
@@ -74,7 +99,7 @@ class HomePage extends StatelessWidget {
           crossAxisCount: 2,
           padding: EdgeInsets.all(16.0),
           childAspectRatio: 8.0 / 9.0,
-          children: _buildGridCards(10) // Replace
+          children: _buildGridCards(context) // Changed code
           ),
       resizeToAvoidBottomInset: false,
     );
