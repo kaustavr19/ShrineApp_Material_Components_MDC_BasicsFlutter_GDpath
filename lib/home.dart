0f700575882b6e3845db3c8b259bb169ff94e317
kaustavr19 <kaustavr19@users.noreply.github.com>
2021-02-02 21:50:52 +0530

    MDC-102 title added to appbar

--- lib/home.dart
+++ lib/home.dart
@@ -4,7 +4,9 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
+      appBar: AppBar(
+        title: Text('SHRINE'),
+      ),
       body: Center(
         child: Text('You did it!'),
       ),
