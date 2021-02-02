614bb7c7efdf2bb82de436f0d53d6ef033cc7157
kaustavr19 <kaustavr19@users.noreply.github.com>
2021-02-02 22:09:55 +0530

    MDC-102 content layouting done

--- lib/home.dart
+++ lib/home.dart
@@ -41,7 +41,31 @@ class HomePage extends StatelessWidget {
         padding: EdgeInsets.all(16.0),
         childAspectRatio: 8.0 / 9.0,
         children: <Widget>[
-          Card(),
+          Card(
+            clipBehavior: Clip.antiAlias,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                AspectRatio(
+                  aspectRatio: 18.0 / 11.0,
+                  child: Image.asset('assets/diamond.png'),
+                ),
+                Padding(
+                  padding: EdgeInsets.fromLTRB(16.0, 12.0, 16.0, 8.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Text('Title'),
+                      SizedBox(
+                        height: 8.0,
+                      ),
+                      Text('Secondary Text'),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
         ],
       ),
       resizeToAvoidBottomInset: false,
