a34f8eaeee91eb8e04b09702841837b0207a6d93
kaustavr19 <kaustavr19@users.noreply.github.com>
2021-02-04 21:18:30 +0530

    MDC-104 front-layer shape added

--- lib/backdrop.dart
+++ lib/backdrop.dart
@@ -28,7 +28,35 @@ class Backdrop extends StatefulWidget {
   _BackdropState createState() => _BackdropState();
 }
 
-// TODO: Add _FrontLayer class (104)
+class _FrontLayer extends StatelessWidget {
+  // TODO: Add on-tap callback (104)
+  const _FrontLayer({
+    Key key,
+    this.child,
+  }) : super(key: key);
+
+  final Widget child;
+
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      elevation: 16.0,
+      shape: BeveledRectangleBorder(
+        borderRadius: BorderRadius.only(topLeft: Radius.circular(46.0)),
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          // TODO: Add a GestureDetector (104)
+          Expanded(
+            child: child,
+          ),
+        ],
+      ),
+    );
+  }
+}
+
 // TODO: Add _BackdropTitle class (104)
 class _BackdropState extends State<Backdrop>
     with SingleTickerProviderStateMixin {
@@ -38,12 +66,15 @@ class _BackdropState extends State<Backdrop>
 
   // TODO: Add BuildContext and BoxConstraints parameters to _buildStack (104)
   Widget _buildStack() {
+    // TODO: Create a RelativeRectTween Animation (104)
+
     return Stack(
       key: _backdropKey,
       children: <Widget>[
-        // TODO: Wrap backLayer in an ExcludeSemantics widget (104)
         widget.backLayer,
-        widget.frontLayer,
+        // TODO: Add a PositionedTransition (104)
+        // TODO: Wrap front layer in _FrontLayer (104)
+        _FrontLayer(child: widget.frontLayer),
       ],
     );
   }
