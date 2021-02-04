afeea59277dccb264c23ca6810b7f0da8158df17
kaustavr19 <kaustavr19@users.noreply.github.com>
2021-02-04 21:04:07 +0530

    MDC-103 gridview replaced with assymetric view

--- lib/home.dart
+++ lib/home.dart
@@ -1,110 +1,43 @@
 import 'package:flutter/material.dart';
-import 'package:intl/intl.dart';
+
 import 'model/products_repository.dart';
 import 'model/product.dart';
+import 'supplemental/assymetric_view.dart';
 
 class HomePage extends StatelessWidget {
+  // TODO: Add a variable for Category (104)
+
   @override
-  List<Card> _buildGridCards(BuildContext context) {
-    List<Product> products = ProductsRepository.loadProducts(Category.all);
-
-    if (products == null || products.isEmpty) {
-      return const <Card>[];
-    }
-
-    final ThemeData theme = Theme.of(context);
-    final NumberFormat formatter = NumberFormat.simpleCurrency(
-        locale: Localizations.localeOf(context).toString());
-
-    return products.map((product) {
-      return Card(
-        clipBehavior: Clip.antiAlias,
-        elevation: 0.0,
-        child: Column(
-          // TODO: Center items on the card (103)
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            AspectRatio(
-              aspectRatio: 18 / 11,
-              child: Image.asset(
-                product.assetName,
-                package: product.assetPackage,
-                // TODO: Adjust the box size (102)
-                fit: BoxFit.fitWidth,
-              ),
-            ),
-            Expanded(
-              child: Padding(
-                padding: EdgeInsets.fromLTRB(16.0, 12.0, 16.0, 8.0),
-                child: Column(
-                  // TODO: Align labels to the bottom and center (103)
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  // TODO: Change innermost Column (103)
-                  children: <Widget>[
-                    // TODO: Handle overflowing labels (103)
-                    Text(
-                      product == null ? '' : product.name,
-                      style: theme.textTheme.button,
-                      softWrap: false,
-                      overflow: TextOverflow.ellipsis,
-                      maxLines: 1,
-                    ),
-                    SizedBox(height: 4.0),
-                    Text(
-                      product == null ? '' : formatter.format(product.price),
-                      style: theme.textTheme.caption,
-                    ),
-                  ],
-                ),
-              ),
-            ),
-          ],
-        ),
-      );
-    }).toList();
-  }
-
   Widget build(BuildContext context) {
+    // TODO: Return an AsymmetricView (104)
+    // TODO: Pass Category variable to AsymmetricView (104)
     return Scaffold(
       appBar: AppBar(
+        brightness: Brightness.light,
+        leading: IconButton(
+          icon: Icon(Icons.menu),
+          onPressed: () {
+            print('Menu button');
+          },
+        ),
         title: Text('SHRINE'),
         actions: <Widget>[
           IconButton(
-            icon: Icon(
-              Icons.search,
-              semanticLabel: 'search',
-            ),
+            icon: Icon(Icons.search),
             onPressed: () {
-              print('Search Button');
+              print('Search button');
             },
           ),
           IconButton(
-            icon: Icon(
-              Icons.tune,
-              semanticLabel: 'filter',
-            ),
+            icon: Icon(Icons.tune),
             onPressed: () {
-              print('Filter Button');
+              print('Filter button');
             },
           ),
         ],
-        leading: IconButton(
-          icon: Icon(
-            Icons.menu,
-            semanticLabel: 'menu',
-          ),
-          onPressed: () {
-            print('Menu Button');
-          },
-        ),
       ),
-      body: GridView.count(
-          crossAxisCount: 2,
-          padding: EdgeInsets.all(16.0),
-          childAspectRatio: 8.0 / 9.0,
-          children: _buildGridCards(context) // Changed code
-          ),
-      resizeToAvoidBottomInset: false,
+      body: AsymmetricView(
+          products: ProductsRepository.loadProducts(Category.all)),
     );
   }
 }
